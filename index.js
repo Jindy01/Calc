e22bc0eef915136ec1c531522348518d8fd88323
Jindy01 <125122479+Jindy01@users.noreply.github.com>
2023-02-17 20:55:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let ranningTotal = 0;
+let buffer = "0";
+let previousOperator = 0 ;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+        else{
+            
+        }
+    }
+}
